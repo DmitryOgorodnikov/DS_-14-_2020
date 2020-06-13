b25af43a00e6d943616b7d4cd1d1fb3f5e55f773
DmitryOgorodnikov <65391963+DmitryOgorodnikov@users.noreply.github.com>
2020-06-13 15:21:51 +0300

    Add files via upload

---
+++ serverW3/serverW3/Brands.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverW3
+{
+    [Serializable]
+    public class Brands
+    {
+        private String name;
+        private String description;
+
+        public Brands()
+        {
+            this.name = "";
+            this.description = "";
+        }
+
+        public Brands(String name, String description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+
+        public String getDescription()
+        {
+            return description;
+        }
+
+        public void setDescription(String description)
+        {
+            this.description = description;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        } 
+    }
+}
